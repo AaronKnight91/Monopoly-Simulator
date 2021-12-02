e07dd75db0a0f73dc9c988c98868e3b1750bdb63
Dr. Aaron Knight <a.knight14@hotmail.com>
2021-12-02 19:33:08 +0000

    Creating a basic player and dice roll that moves around a board

---
+++ run.py
@@ -0,0 +1,51 @@
+from random import randint
+
+class DiceRoll():
+
+    def __init__(self):
+
+        self._dice_1 = self.roll_die()
+        self._dice_2 = self.roll_die()
+
+        self.roll = self.sum_die()
+
+        self.double_roll = self.check_double_roll()
+
+    def roll_die(self):
+
+        return randint(1, 7)
+
+    def sum_die(self):
+
+        return self._dice_1 + self._dice_2
+
+    def check_double_roll(self):
+
+        if self._dice_1 == self._dice_2:
+            return True
+        else:
+            return False
+
+class Player():
+
+    def __init__(self):
+
+        self.position = 0
+
+    def update_position(self, dice_roll):
+
+        self.position += dice_roll
+        if self.position % 39 > 0:
+            self.position = self.position % 40
+
+    
+def main():
+
+    player = Player()
+
+    for i in range(10):
+        dice = DiceRoll()
+        player.update_position(dice.roll)
+
+if __name__ == "__main__":
+    main()
