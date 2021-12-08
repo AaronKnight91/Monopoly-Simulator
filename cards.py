1870635846f2a885e1b1cf9c8e59e52951266a8b
Dr. Aaron Knight <a.knight14@hotmail.com>
2021-12-08 19:41:32 +0000

    Adding classes to handle drawing and playing of cards in the game

---
+++ cards.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+class Cards():
+
+    def __init__(self, file):
+
+        self.cards = self.get_card_deck(file)
+        
+    def get_cards_deck(file):
+        
+        return pd.read_csv(file)
+
+    def shuffle_cards(self):
+
+        self.cards["Cards"](frac=1).reset_index(drop=True)
+
+    def draw_card(self):
+
+        tmp = self.card["Cards"].iloc[0]
+        self.card["Cards"].drop(self.card["Cards"].iloc[0], inplace=True)
+        return tmp
+
+    def replace_card(self, card):
+
+        self.cards.append(card)
+
+class ChanceCards(Cards):
+
+    def __init__(self, file):
+
+        Cards.__init__(self)
+
+class CommunityChestCards(cards):
+
+    def __init__(self, file):
+
+        Cards.__init__(self)
